df03a98c180f747c3113f651a580b68ca5a91de0
Arthur Piminov <artyr26@hotmail.com>
2023-09-26 11:43:44 +0700

    lab 1.4 done

--- src/clojure_lab_1/lab1_4.clj
+++ src/clojure_lab_1/lab1_4.clj
@@ -1,8 +1,27 @@
 (ns clojure-lab-1.lab1-4)
 
-(defn combs [lst n]
-  (map (fn [x]
-         (conj (first lst) x)) lst))
+(def similiar?
+  (fn [x]
+    (loop [lst x
+           acc (rest x)
+           res true]
+      (if (or (= res false) (empty? lst))
+        res
+        (recur (rest lst) (rest acc) (if (= (first lst) (first acc))
+                                       false
+                                       true))))))
 
+(defn alphabet_combinations [lst n]
+  (filter similiar? (loop [lst1 lst
+                           lst2 lst
+                           cnt n
+                           acc nil]
+                      (if (zero? cnt)
+                        acc
+                        (recur
+                         (reduce concat '() (reduce concat '() (map (fn [x]
+                                                                      (map (fn [y]
+                                                                             (conj nil (str x y))) lst2)) lst1)))
+                         lst2 (dec cnt) lst1)))))
 
-(combs '(1 2 3 4) 3)
+(alphabet_combinations '("a" "b" "c" ) 3)
